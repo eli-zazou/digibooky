10b66960334b5743880d08353887e835a44f50aa
Josquin <josquin.dev@hotmail.com>
2023-10-18 11:29:36 +0200

    BookRepository added

--- src/main/java/com/switchfully/www/repository/BookRepository.java
+++ src/main/java/com/switchfully/www/repository/BookRepository.java
@@ -1,4 +1,24 @@
 package com.switchfully.www.repository;
 
+import com.switchfully.www.domain.Book;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 public class BookRepository {
+
+    private final Map<String, Book> booksById;
+
+    public BookRepository(){
+        this.booksById = new HashMap<>();
+    }
+
+    public Optional<Book> getById(String Id){
+        return null;
+    }
+
+    
+
+
 }
