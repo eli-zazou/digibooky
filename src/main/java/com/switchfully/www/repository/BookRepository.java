77d0b89dff4cf5f744595df9a0dc6d2d01d644e8
Gabriel Desiderio <desiderio.gabriel@protonmail.com>
2023-10-18 13:13:40 +0200

    getByAuthor

--- src/main/java/com/switchfully/www/repository/BookRepository.java
+++ src/main/java/com/switchfully/www/repository/BookRepository.java
@@ -3,8 +3,10 @@ package com.switchfully.www.repository;
 import com.switchfully.www.domain.Book;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class BookRepository {
 
@@ -16,6 +18,10 @@ public class BookRepository {
 
     public Optional<Book> getById(String Id){
         return null;
+    }
+
+    public Optional<List<Book>> getByAuthor(String author) {
+        return Optional.of(booksById.values().stream().filter(book -> book.getAuthor().getLastname().equals(author)).collect(Collectors.toList()));
     }
 
     
