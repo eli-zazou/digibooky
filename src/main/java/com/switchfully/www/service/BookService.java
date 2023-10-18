11fe821cca16a3778fd7188e868241410aa9c2a6
Josquin <josquin.dev@hotmail.com>
2023-10-18 14:32:34 +0200

    testing CreateBookDto

--- src/main/java/com/switchfully/www/service/BookService.java
+++ src/main/java/com/switchfully/www/service/BookService.java
@@ -26,11 +26,4 @@ public class BookService {
         return bookMapper.mapToDTO(books);
 
     }
-
-    public BookDto getBookById(String id) {
-        return bookMapper.mapToDTO(
-                bookRepository
-                        .getById(id)
-                        .orElseThrow(() -> new IllegalArgumentException("No Book could be found for id " + id)));
-    }
 }
