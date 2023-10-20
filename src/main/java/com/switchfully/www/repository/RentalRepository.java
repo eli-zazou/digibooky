906d87ae971f2f8b65e29768b56eaaf4eba16460
Josquin <josquin.dev@hotmail.com>
2023-10-20 16:07:35 +0200

    Get OverdueBooks

--- src/main/java/com/switchfully/www/repository/RentalRepository.java
+++ src/main/java/com/switchfully/www/repository/RentalRepository.java
@@ -14,28 +14,35 @@ import java.util.Optional;
 public class RentalRepository {
     private Map<String, Rental> rentalById;
 
-    private RentalRepository () {
+    private RentalRepository() {
         this.rentalById = new HashMap<>();
     }
 
     public Rental addRental(Rental rental) {
 
-        rentalById.put(rental.getId(),rental);
+        rentalById.put(rental.getId(), rental);
         return rental;
     }
 
     public Boolean removeRental(Rental rental) {
-        rentalById.remove(rental.getId(),rental);
+        rentalById.remove(rental.getId(), rental);
         return true;
     }
 
-    public Collection<Rental> getRentalByMember(String id){
+    public Collection<Rental> getRentalByMember(String id) {
         return rentalById.values()
                 .stream()
                 .filter(rental -> rental.getMember().getId().equals(id))
                 .toList();
     }
 
+    public List<Book> getOverdueBooks(){
+        return rentalById.values()
+                .stream()
+                .filter(Rental::isOverDue)
+                .map(Rental::getBook)
+                .toList();
+    }
     public Optional<Rental> getById(String Id) {
         return Optional.ofNullable(rentalById.get(Id));
     }
